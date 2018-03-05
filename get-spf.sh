aaaa4852889850070c2eed5fb28cd5e33815aae2
Uwe Sommer <uwe@usommer.de>
2018-03-05 21:54:16 +0100

    bugs fixed
    fixed  " " breaks in txt records

--- get-spf.sh
+++ get-spf.sh
@@ -1,68 +1,63 @@
-#!/bin/bash
-# spf parser to grab netblocks (ipv4) 
-# for domains in a given list (file)
-# 
-# read "a:" statements
-# read "ipv4:" statements
-# read "include:" statements and follow up to 4 level
-# read "domain" a and mx records
+getspf() {
+# get all ipv4 ips from SPF record including mx and domain IPs
+# usage: "getspf domain" or "getspf file" (list of domains)
 #
-# Uwe Sommer
-# uwe@usommer.de
-# 12/2017
-
-## get mx records and domain a records
-## filter for valid ip4 addresses:
-## awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
-
-# main function
-getspf()
-{
-## input domainlist
-file=$1
+# uncomment debug switch:
+# set -x
+## default parameter for IP Lists from domains for creating a postscreen whitelist
+file="$1"
 if [ "$#" -le "0" ]; then
 	echo "Domainlist or domain argument required"
 	return 1
 fi
-if [ ! -f $1 ]; then
-	echo $1 > /tmp/domain
+## move domain parameter fo file
+if [ ! -f "$1" ]; then
+	echo "$1" > /tmp/domain
 	file=/tmp/domain 
 fi
+## functions to reuse code
+getrecords () {
+    awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|include:|a:/) {print $i}}}' | sed 's/\"\ \"//g' |sed s/\"//g
+}
+getip4() {
+    grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g 
+}
+getinclude() {
+    grep -e "^include:" |awk -F":" '{print $2}' |xargs dig txt +short |grep "v=spf" 
+}
+get_a() {
+    grep -e "^a:" |awk -F":" '{print $2}'
+}
+validate() {
+    awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
+}
+## main spf query part
 # get domain a records
-domain=$(xargs dig +short < "$file" |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./')
+domain=$(xargs dig +short < "$file" |validate)
 echo "$domain"
 # get domain mx records
-mxips=$(xargs dig mx +short < "$file" | awk '{print $2}'| xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./')
+mxips=$(xargs dig mx +short < "$file" | awk '{print $2}'| xargs dig +short |validate)
 echo "$mxips"
-
-# spf queries
+## spf queries
 # remove quotes from output and validate ipv4
-results1=$(xargs dig txt +short < "$file" |grep "v=spf" | awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|^include:|^a:/) {print $i}}}')
-echo "$results1" |grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g 
-echo "$results1" |grep -e "^a:" |awk -F":" '{print $2}' |xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
-
+results1=$(xargs dig txt +short < "$file" |grep "v=spf" | getrecords) 
+echo "$results1" |getip4 |validate
+echo "$results1" |get_a |xargs dig +short |validate
 # include queries
-results2=$(echo "$results1" |grep -e "^include:" |awk -F":" '{print $2}' |xargs dig txt +short |grep "v=spf" | awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|include:|a:/) {print $i}}}' |sed 's/\"\ \"//g'| sed s/\"//g ) 
-echo "$results2" |grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g
-echo "$results2" |grep -e "^a:" |awk -F":" '{print $2}' |xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
-
+results2=$(echo "$results1" |getinclude| getrecords) 
+echo "$results2" |getip4 |validate
+echo "$results2" |get_a |xargs dig +short |validate
 # level2 include queries
-results3=$(echo "$results2" |grep -e "^include:" |awk -F":" '{print $2}' |xargs dig txt +short |grep "v=spf" | awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|include:|a:/) {print $i}}}' |sed 's/\"\ \"//g'| sed s/\"//g) 
-echo "$results3" |grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g
-echo "$results3" |grep -e "^a:" |awk -F":" '{print $2}' |xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
-
+results3=$(echo "$results2" |getinclude| getrecords) 
+echo "$results3" |getip4 |validate
+echo "$results3" |get_a |xargs dig +short |validate
 # level3 include queries
-results4=$(echo "$results3" |grep -e "^include:" |awk -F":" '{print $2}' |xargs dig txt +short |grep "v=spf" | awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|include:|a:/) {print $i}}}' |sed 's/\"\ \"//g'| sed s/\"//g)
-echo "$results4" |grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g
-echo "$results4" |grep -e "^a:" |awk -F":" '{print $2}' |xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
-
+results4=$(echo "$results3" |getinclude| getrecords)
+echo "$results4" |getip4 |validate
+echo "$results4" |get_a |xargs dig +short |validate
 # level4 include queries
-results5=$(echo "$results4" |grep -e "^include:" |awk -F":" '{print $2}' |xargs dig txt +short |grep "v=spf" | awk '{for(i=1;i<=NF;i++){if ($i ~ /^ip4:|include:|a:/) {print $i}}}' |sed 's/\"\ \"//g'| sed s/\"//g)
-echo "$results5" |grep -e "^ip4:" | awk -F":" '{print $2}' |sed s/\"//g
-echo "$results5" |grep -e "^a:" |awk -F":" '{print $2}' |xargs dig +short |awk -F'.' 'NF==4 && $1 > 0 && $1<256 && $2<256 && $3<256 && $4<256 && !/\.\./'
+results5=$(echo "$results4" |getinclude| getrecords)
+echo "$results5" |getip4 |validate
+echo "$results5" |get_a |xargs dig +short |validate
 }
-
-
-# remove duplicate entries and write postfix mapsfile
-getspf "$1" |sort |uniq | awk '{print $1"  permit"}' > postscreen-whitelists-cidr
-cat postscreen-whitelists-cidr
+getspf "$1"
